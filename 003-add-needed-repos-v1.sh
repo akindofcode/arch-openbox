9188e2f8326abeddd8861259a8f85e3bccb6c5fc
Chema Pérez <akindofcode@gmail.com>
2017-06-27 13:41:38 +0000

    added sublime-text repo

--- 003-add-needed-repos-v1.sh
+++ 003-add-needed-repos-v1.sh
@@ -6,9 +6,20 @@
 ##################################################################################################################
 
 # backup pacman.conf
+
 sudo cp /etc/pacman.conf /etc/pacman.conf.old
 
 # add herecura repo (vivaldi)
+
 sudo sed -i /etc/pacman.conf -e '$a\\n[herecura]\nServer = http://repo.herecura.be/$repo/$arch'
+
+# add sublime text repo
+
+curl -O https://download.sublimetext.com/sublimehq-pub.gpg && sudo pacman-key --add sublimehq-pub.gpg && sudo pacman-key --lsign-key 8A8F901A && rm sublimehq-pub.gpg
+
+# Stable
+# echo -e "\n[sublime-text]\nServer = https://download.sublimetext.com/arch/stable/x86_64" | sudo tee -a /etc/pacman.conf
+# Dev
+echo -e "\n[sublime-text]\nServer = https://download.sublimetext.com/arch/dev/x86_64" | sudo tee -a /etc/pacman.conf
 
 sudo pacman -Syuu
