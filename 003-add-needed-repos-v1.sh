775115fb72a82cbd83906ecd730e1237762ee05a
akindofcode <akindofcode@gmail.com>
2017-06-27 15:36:02 +0200

    added permissions

--- 003-add-needed-repos-v1.sh
+++ 003-add-needed-repos-v1.sh
@@ -5,12 +5,10 @@
 # Author 	: 	akindofcode
 ##################################################################################################################
 
-#sudo sed -i /etc/pacman.conf -e '$a\\n[herecura]\nServer = http://repo.herecura.be/herecura/x86_64\n\n[repo-ck]\nServer = http://repo-ck.com/$arch'
+# backup pacman.conf
+sudo cp /etc/pacman.conf /etc/pacman.conf.old
 
+# add herecura repo (vivaldi)
 sudo sed -i /etc/pacman.conf -e '$a\\n[herecura]\nServer = http://repo.herecura.be/$repo/$arch'
-
-#sudo pacman-key -r 5EE46C4C
-
-#sudo pacman-key --lsign-key 5EE46C4C
 
 sudo pacman -Syuu
