67b7542aef15a77416c08f2e5ee296845a4f26f2
Chema Pérez <akindofcode@gmail.com>
2017-06-28 21:33:04 +0000

    005-install-openbox-utils-v2.sh created online with Bitbucket

---
+++ 005-install-openbox-utils-v2.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+set -e
+#======================================================================================
+#                                
+# Author  : akindofcode
+# License : Distributed under the terms of GNU GPL version 2 or later
+# 
+#======================================================================================
+
+cp -R /etc/xdg/openbox ~/.config
+
+###############################################################################################
+
+#obconf is gtk2 and not needed if lxappearance-obconf-gtk3 installed ?? 
+#sudo pacman -S --noconfirm --needed obconf
+
+sudo pacman -S --noconfirm --needed lxappearance-gtk3 lxappearance-obconf-gtk3 noto-fonts noto-fonts-emoji
+
+packer -S --noconfirm --noedit neofetch
+
+packer -S --noconfirm --noedit obmenu3
+
+packer -S --noconfirm --noedit obmenu-generator
+
+packer -S --noconfirm --noedit obkey
+
+###############################################################################################
+
+#Modify obmenu-generator config files (config y schema)
+
+#Generate static menu with icons
+obmenu-generator -s -i
+
+# xfce-slimlock
+
+###############################################################################################
+
+echo "################################################################"
+echo "###################    openbox utils installed  ################"
+echo "################################################################"
+
+#arc themes and icons
+packer -S --noconfirm --noedit arc-gtk-theme archlabs-arch-themes-git
+
+sh 010-icons-sardi-v3.sh
+
+packer -S --noconfirm --noedit ttf-font-awesome breeze-snow-cursor-theme
+
+echo "################################################################"
+echo "#############  eye candy software  installed   #################"
+echo "################################################################"
