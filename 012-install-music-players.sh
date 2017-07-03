624c49ca62eebe6626630c2b66f2ac329d3eb320
Chema Pérez <akindofcode@gmail.com>
2017-07-03 10:24:59 +0200

    added mpd configuration

--- 012-install-music-players.sh
+++ 012-install-music-players.sh
@@ -10,14 +10,20 @@ set -e
 # installation of mpd
 sudo pacman -S --noconfirm --needed mpd mpc ncmpcpp
 
+# create directories to store data
+mkdir ~/.local/share/mpd
+mkdir ~/Playlists
+
 #sudo pacman -S --noconfirm --needed ncmpcpp
 
 sudo pacman -S --noconfirm --needed deadbeef
 
 # wine wineaiso foobar
 
+# start mpd
 mpd ~/.config/mpd/mpd.conf &
 
+# add to autostart
 echo "mpd ~/.config/mpd/mpd.conf &" >> ~/.config/openbox/autostart
 
 
