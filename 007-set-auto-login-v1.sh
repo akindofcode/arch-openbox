7d0eac0b44038706026cdd2336e16ca65df486db
Chema Pérez <akindofcode@gmail.com>
2017-07-04 09:50:15 +0200

    added auto-login script

--- 007-set-auto-login-v1.sh
+++ 007-set-auto-login-v1.sh
@@ -12,10 +12,13 @@ sudo groupadd -r nopasswdlogin
 # add user to autologin and nopasswdlogin groups
 sudo gpasswd -a chema autologin
 sudo gpasswd -a chema nopasswdlogin
- 
+
 # if adding to the config files doesn't work, let's try to copy the modified files
+
+sudo cp /etc/pam.d/lightdm /etc/pam.d/lightdm.old
 echo "auth        sufficient  pam_succeed_if.so user ingroup nopasswdlogin" | sudo tee --append /etc/pam.d/lightdm > /dev/null
 
+sudo cp /etc/lightdm/lightdm.conf /etc/lightdm/lightdm.conf.old
 echo '[Seat:*]' | sudo tee --append /etc/lightdm /lightdm.conf > /dev/null
 echo 'autologin-user= chema' | sudo tee --append /etc/lightdm /lightdm.conf > /dev/null
 
