cf17708ac1e570f391e68e5e580cdfc82382305f
akindofcode <akindofcode@gmail.com>
2018-03-14 14:40:51 +0100

    Modified for clarity

--- installation/001-install-fastest-arch-mirrors-and-update-v1.sh
+++ installation/001-install-fastest-arch-mirrors-and-update-v1.sh
@@ -8,32 +8,30 @@ set -e
 #======================================================================================
 
 echo "################################################################"
-echo "####             Installing reflector                        ###"
+echo "####                Installing reflector                     ###"
 echo "################################################################"
 
 # installing refector to test wich servers are fastest
 sudo pacman -S --noconfirm --needed reflector
 
 echo "################################################################"
-echo "####   finding fastest servers be patient for the world      ###"
+echo "####              finding fastest servers                    ###"
 echo "################################################################"
-
-# finding the fastest archlinux servers
 
 sudo reflector -l 100 -f 50 --sort rate --threads 5 --verbose --save /tmp/mirrorlist.new && rankmirrors -n 0 /tmp/mirrorlist.new > /tmp/mirrorlist && sudo cp /tmp/mirrorlist /etc/pacman.d
 
 echo "################################################################"
-echo "####       fastest servers  saved                            ###"
+echo "####                adding fastest servers 			 	   ###"
 echo "################################################################"
 
 cat /etc/pacman.d/mirrorlist
 
 echo "################################################################"
-echo "####              Checking for possible updates             ####"
+echo "####             Checking for possible updates              ####"
 echo "################################################################"
 
 sudo pacman -Syyu --noconfirm
 
 echo "################################################################"
-echo "#####         Your system is now up to date                 ####"
+echo "#####                 System up to date                     ####"
 echo "################################################################"
