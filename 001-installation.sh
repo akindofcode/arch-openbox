8d935edea1c834900e99df5ab2d1bb1ac885433d
akindofcode <akindofcode@gmail.com>
2018-03-15 09:42:45 +0100

    Fixed script names

--- 001-installation.sh
+++ 001-installation.sh
@@ -7,19 +7,19 @@ set -e
 # 
 #======================================================================================
 
-sh installation/001-install-fastest-arch-mirrors-and-update-v1.sh
-sh installation/002-install-packer-for-aur-make-build-v1.sh
-sh installation/003-add-needed-repos-v1.sh
-sh installation/004-install-openbox-utils-v2.sh
-sh installation/005-install-core-software-v2.sh
-sh installation/006-install-applications-v1.sh
-sh installation/007-set-auto-login-v1.sh
-sh installation/008-install-fonts-v1.sh
-sh installation/009-install-arc-theme-v1.sh
-sh installation/010-install-sardi-icons-v3.sh
-sh installation/011-install-calibre-v1.sh
-sh installation/012-install-music-players-v1.sh
-sh installation/013-install-zsh-v1.sh
+sh installation/001-install-fastest-arch-mirrors-and-update.sh
+sh installation/002-install-packer-for-aur-make-build.sh
+sh installation/003-add-needed-repos.sh
+sh installation/004-install-openbox-utils.sh
+sh installation/005-install-core-software.sh
+sh installation/006-install-applications.sh
+sh installation/007-set-auto-login.sh
+sh installation/008-install-fonts.sh
+sh installation/009-install-arc-theme.sh
+sh installation/010-install-sardi-icons.sh
+sh installation/011-install-calibre.sh
+sh installation/012-install-music-players.sh
+sh installation/013-install-zsh.sh
 
 echo "################################################################"
 echo "#####################   installation done !!!  #################"
