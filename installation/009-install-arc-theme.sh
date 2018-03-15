593be94709e5c0270fceecfc0941d194f78e9cb6
akindofcode <akindofcode@gmail.com>
2018-03-15 16:32:51 +0100

    modified

--- installation/009-install-arc-theme.sh
+++ installation/009-install-arc-theme.sh
@@ -8,11 +8,9 @@ set -e
 #======================================================================================
 
 #arc themes and icons
-packer -S --noconfirm --noedit arc-gtk-theme archlabs-arch-themes-git
+packer -S --noconfirm --noedit arc-gtk-theme
 
-## packer -S --noconfirm --noedit breeze-snow-cursor-theme
-packer -S --noconfirm --noedit default-snow-cursor-theme
+# packer -S --noconfirm --noedit breeze-snow-cursor-theme	
+# packer -S --noconfirm --noedit default-snow-cursor-theme
 
-echo "################################################################"
-echo "###################  arc themes installed  #####################"
-echo "################################################################"
+echo "### arc theme installed ###"
